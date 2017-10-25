f34591a739b8badbdd19f003ccdb4c5b7102b7ce
Markus Isaksson <markus.isaksson@gmail.com>
2017-10-25 13:36:01 +0200

    Behavioral cloning - initialize project
    - Adds project files from Udacity repo.
    - Implements a setup script to download simulator and the example
      training data.
    - Also renamed previous projects to simpler names.

---
+++ term1/P3-Behavioral-Cloning/setup.py
@@ -0,0 +1,23 @@
+import zipfile
+import urllib.request
+import os.path
+
+
+def download_and_unzip(input_url, output_dir):
+    if os.path.exists(output_dir):
+        print("The output dir {} already exist, skipping download.".format(output_dir))
+    else:
+        print("Downloading file {}".format(input_url))
+        filehandle, _ = urllib.request.urlretrieve(input_url)
+
+        print("Unzipping file to {}".format(output_dir))
+        zipfile.ZipFile(filehandle).extractall(output_dir)
+
+
+input_url = "https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip"
+output_dir = "./data/"
+download_and_unzip(input_url, output_dir)
+
+input_url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/February/58ae46bb_linux-sim/linux-sim.zip"
+output_dir = "./simulator/"
+download_and_unzip(input_url, output_dir)
