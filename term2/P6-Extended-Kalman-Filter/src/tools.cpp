12488ab49128abff0eccdc376bfd29cc8b6d5aba
Markus Isaksson <markus.isaksson@gmail.com>
2018-01-11 12:11:15 +0100

    Calculates the Root Mean Squared Error (RMSE)
    Use RMSE as performance measure for the tracking algorithm.

--- term2/P6-Extended-Kalman-Filter/src/tools.cpp
+++ term2/P6-Extended-Kalman-Filter/src/tools.cpp
@@ -12,10 +12,30 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+   * Calculates the Root Mean Squared Error (RMSE).
+   */
+   VectorXd rmse(4);
+   rmse << 0,0,0,0;
 
+   if ((estimations.size() == 0) || (estimations.size() != ground_truth.size()))
+   {
+      cout << "CalculateRMSE: Invalid input" << endl;
+   }
+   else
+   {
+      // Accumulate squared residuals
+      for(int i = 0; i < estimations.size(); ++i){
+         VectorXd error = (estimations[i] - ground_truth[i]);
+         rmse = rmse.array() + error.array() * error.array();
+      }
+
+      // Calculate the mean.
+      rmse /= estimations.size();
+
+      // Calculate the squared root.
+      rmse = rmse.array().sqrt();
+   }
+   return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
