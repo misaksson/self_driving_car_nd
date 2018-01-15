deb4d71d72f763aa0058ce206307f35df5c0a3a3
Markus Isaksson <markus.isaksson@gmail.com>
2018-01-15 15:47:35 +0100

    Remove Kalman filter init method
    The Kalman filter init method was never put to use so it can be
    removed. All state variables are instead public and setup and
    accessed directly by the tracker class FusionEKF.

--- term2/P6-Extended-Kalman-Filter/src/kalman_filter.cpp
+++ term2/P6-Extended-Kalman-Filter/src/kalman_filter.cpp
@@ -9,16 +9,6 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
-}
-
 void KalmanFilter::Predict() {
   // Predict the new state estimation.
   x_ = F_ * x_;
